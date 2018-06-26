4d707a434a751873c7ee37dc7e6d631db71c0a60
voyage4301 <815717711@qq.com>
2018-06-26 20:20:14 +0800

    二级分类初步功能完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -13,7 +13,6 @@ $(function () {
       },
       dataType: 'json',
       success: function (info) {
-        console.log(info);
         var total = Math.ceil(info.total / info.size)
         $('.lt_content tbody').html(template('tmp', info))
         $('.lt_content #pagintor').bootstrapPaginator({
@@ -34,13 +33,109 @@ $(function () {
 
   //点击显示模态框
   $('.lt_content .btn').on('click', function () {
+    //显示模态框
     $('#myModal1').modal('show')
+    //获取一级分类的数据
+    $.ajax({
+      type: 'get',
+      url: '/category/queryTopCategoryPaging',
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      dataType: 'json',
+      success: function (info) {
+        //动态生成下拉选项
+        $('.select-menu').html(template('first-tmp', info))
+        //下拉选项点击事件
+        $('.select-menu li a').each(function () {
+          $(this).on('click', function () {
+            $('#categoryId').val($(this).data('id'))
+            $('#form').data('bootstrapValidator').updateStatus('categoryId', 'VALID')
+            $('.dropdownText').text($(this).text())
+          })
+        })
+      }
+    })
   })
+
+
+
 
   //添加图片
   $('#myModal1 .btn-addImg').on('click', function () {
-    console.log('哈哈');
     $("[type='file']").click()
-
   })
+
+  //图片上传回显
+  $('#fileupload').fileupload({
+    dataType: 'json',
+    done: function (e, data) {
+      console.log(data);
+      $('.img-box img').attr('src', data.result.picAddr)
+      $('[name="brandLogo"]').val(data.result.picAddr)
+      $('#form').data('bootstrapValidator').updateStatus('brandLogo', 'VALID')
+    }
+  })
+
+  //表单验证
+  $('#form').bootstrapValidator({
+    //1.指定不校验的类型
+    excluded: [],
+    //2.设置校验的字体图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok', //校验成功
+      invalid: 'glyphicon glyphicon-remove', //校验失败
+      validating: 'glyphicon glyphicon-refresh' //校验中
+    },
+    //3.设置规则
+    fields: {
+      categoryId: {
+        validators: {
+          notEmpty: {
+            message: '请您选择一级分类!'
+          }
+        }
+      },
+      brandName: {
+        validators: {
+          notEmpty: {
+            message: '请您输入二级分类名!'
+          }
+        }
+      },
+      brandLogo: {
+        validators: {
+          notEmpty: {
+            message: '请您上传图片!'
+          }
+        }
+      }
+    }
+  })
+
+  //添加二级分类
+  $('#form').on('success.form.bv', function (e) {
+    e.preventDefault()
+    console.log($('#form').serialize())
+    $.ajax({
+      type: 'post',
+      url: '/category/addSecondCategory',
+      data: $('#form').serialize(),
+      dataType: 'json',
+      success: function (info) {
+        console.log(info);
+        if (info.success) {
+          $('#myModal1').modal('hide')
+          $('#form').data('bootstrapValidator').resetForm(true)
+          currentPage = 1
+          render(currentPage)
+
+          $('.dropdownText').text('请选择一级分类')
+          $('.img-box img').attr('src', '../images/default.png')
+        }
+      }
+    })
+  })
+
 })
