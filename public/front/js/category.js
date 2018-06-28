a687839c375a1d125509f5c36f3f3aa7e129b6a4
voyage4301 <815717711@qq.com>
2018-06-28 17:30:43 +0800

    移动端分类页基本完成

---
+++ public/front/js/category.js
@@ -0,0 +1,42 @@
+$(function () {
+
+  // 1.区域滚动
+  mui('.mui-scroll-wrapper').scroll({
+    deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+  });
+
+  //2.动态渲染左侧信息
+  function renderLeft() {
+    $.ajax({
+      type: 'get',
+      url: '/category/queryTopCategory',
+      dataType: 'json',
+      success: function (info) {
+        // console.log(info);
+        $('#firstList').html(template('listFirst', info))
+        $('#firstList').children().eq(0).addClass('current')
+      }
+    })
+  }
+  renderLeft()
+
+
+  //3.动态渲染右侧二级信息
+  function renderRight(id) {
+    $.ajax({
+      type: 'get',
+      url: '/category/querySecondCategory',
+      data: {
+        id: id || 1
+      },
+      dataType: '',
+      success: function (info) {
+        // console.log(info);
+        $('#secondList').html(template('listSecond', info))
+      }
+    })
+  }
+
+  renderRight()
+
+})
